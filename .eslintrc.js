5bba0125ca946cebee8e71caf8c717df3b2a2b10
Brooks Lybrand <lybrand.brooks@heb.com>
2022-02-04 16:10:41 -0600

    Add tooling

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    node: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'prefer-const': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+  },
+}
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  content: ['./app/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'media',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
